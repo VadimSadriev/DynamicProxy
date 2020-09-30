fb225294debf59d3fe42bbfcd2e3814c8a269119
Садриев Вадим <SadrievVF@it2g.ru>
2020-09-30 17:16:24 +0500

    Move replace method to library

--- src/DynamicProxy/DynamicProxy.Tests/CastleDynamicProxyTests.cs
+++ src/DynamicProxy/DynamicProxy.Tests/CastleDynamicProxyTests.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Linq;
 using Castle.DynamicProxy;
+using DynamicProxy.Extensions;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
-using Moq.Internals;
-using Scrutor;
 using Xunit;
 using IInvocation = Castle.DynamicProxy.IInvocation;
 
@@ -100,7 +99,8 @@ namespace DynamicProxy.Tests
             services.AddTransient<IFooService, FooService>();
             services.AddTransient<IInterceptor, LoggerInterceptor>();
 
-            ReplaceWithProxy(services);
+            services.ReplaceWithProxy();
+            
             var serviceProvider = services.BuildServiceProvider();
 
             // act
@@ -110,63 +110,6 @@ namespace DynamicProxy.Tests
             // assert
             proxyFooService.Should().BeAssignableTo<IProxyService>();
         }
-
-        private void ReplaceWithProxy(IServiceCollection services)
-        {
-            // Берём из контейнера все сервисы которые должны перейти в прокси
-            var proxyServiceDescriptors = services
-                .Where(x => x.ServiceType.GetInterfaces().Contains(typeof(IProxyService)))
-                .ToList();
-
-            foreach (var serviceDescriptor in proxyServiceDescriptors)
-            {
-                // удаляем текующую реализацию в контейнере
-                services.Remove(serviceDescriptor);
-
-                // берём первый конструктор (не учитываем кейс с мнонежством конструкторов. нужно заморачиваться)
-                var constructorInfo = serviceDescriptor.ImplementationType.GetConstructors()
-                    .ToList()
-                    .FirstOrDefault();
-
-                var constructorParameters = constructorInfo.GetParameters().ToList();
-
-                // типы зависимостей конструктора для последующего вытаскивания из контейнера
-                var dependencyTypes = constructorParameters.Select(x => x.ParameterType).ToList();
-
-                object ImplFactory(IServiceProvider provider)
-                {
-                    // список всех зависимостей текущего типа в контейнере
-                    // var serviceDependencies = new List<object>();
-                    var serviceDependencies = new object[dependencyTypes.Count];
-
-                    // достаём все зависимости конструктора
-                    for (var i = 0; i < dependencyTypes.Count; i++)
-                    {
-                        var service = provider.GetRequiredService(dependencyTypes[i]);
-                        serviceDependencies[i] = service;
-                    }
-
-                    var interceptors = provider.GetServices<IInterceptor>().ToArray();
-
-                    // Создаём прокси прокидывая зависимости в конструктор
-                    // порядок аргументов должен быть соответствующим сервисам забраным из контейнера
-                    var serviceToProxy = Activator.CreateInstance(serviceDescriptor.ImplementationType, serviceDependencies);
-
-                    var proxy = _proxyGenerator.CreateInterfaceProxyWithTarget(serviceDescriptor.ServiceType, serviceToProxy, interceptors);
-
-                    return proxy;
-                }
-
-                var proxyServiceDescriptor = new ServiceDescriptor(serviceDescriptor.ServiceType, ImplFactory, serviceDescriptor.Lifetime);
-                // добавляем реализацию с прокси
-                services.Add(proxyServiceDescriptor);
-            }
-        }
-    }
-
-
-    public interface IProxyService
-    {
     }
 
     public interface IFooService : IProxyService
---
+++ src/DynamicProxy/DynamicProxy/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using Castle.DynamicProxy;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DynamicProxy.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection ReplaceWithProxy(this IServiceCollection services)
+        {
+            // Берём из контейнера все сервисы которые должны перейти в прокси
+            var proxyServiceDescriptors = services
+                .Where(x => x.ServiceType.GetInterfaces().Contains(typeof(IProxyService)))
+                .ToList();
+
+            foreach (var serviceDescriptor in proxyServiceDescriptors)
+            {
+                // удаляем текующую реализацию в контейнере
+                services.Remove(serviceDescriptor);
+
+                // берём первый конструктор (не учитываем кейс с мнонежством конструкторов. нужно заморачиваться)
+                var constructorInfo = serviceDescriptor.ImplementationType.GetConstructors()
+                    .ToList()
+                    .FirstOrDefault();
+
+                var constructorParameters = constructorInfo.GetParameters().ToList();
+
+                // типы зависимостей конструктора для последующего вытаскивания из контейнера
+                var dependencyTypes = constructorParameters.Select(x => x.ParameterType).ToList();
+
+                object ImplFactory(IServiceProvider provider)
+                {
+                    // список всех зависимостей текущего типа в контейнере
+                    // var serviceDependencies = new List<object>();
+                    var serviceDependencies = new object[dependencyTypes.Count];
+
+                    // достаём все зависимости конструктора
+                    for (var i = 0; i < dependencyTypes.Count; i++)
+                    {
+                        var service = provider.GetRequiredService(dependencyTypes[i]);
+                        serviceDependencies[i] = service;
+                    }
+
+                    var interceptors = provider.GetServices<IInterceptor>().ToArray();
+
+                    // Создаём прокси прокидывая зависимости в конструктор
+                    // порядок аргументов должен быть соответствующим сервисам забраным из контейнера
+                    var serviceToProxy = Activator.CreateInstance(serviceDescriptor.ImplementationType, serviceDependencies);
+
+                    var proxyGenerator = new ProxyGenerator();
+
+                    var proxy = proxyGenerator.CreateInterfaceProxyWithTarget(serviceDescriptor.ServiceType, serviceToProxy, interceptors);
+
+                    return proxy;
+                }
+
+                var proxyServiceDescriptor = new ServiceDescriptor(serviceDescriptor.ServiceType, ImplFactory, serviceDescriptor.Lifetime);
+                // добавляем реализацию с прокси
+                services.Add(proxyServiceDescriptor);
+            }
+
+            return services;
+        }
+    }
+}
